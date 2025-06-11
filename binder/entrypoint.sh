17904ae731d44612776eba1ee52bd8a7094b78bb
srikanth malineni <srikanthmalineni635@gmail.com>
2025-06-11 22:55:16 +0200

    amended

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Initialize conda
+source /opt/conda/etc/profile.d/conda.sh
+conda activate agentic_ai311
+
 # source ros ws
 source ${ROS_WS}/devel/setup.bash
 
@@ -8,7 +12,29 @@ MONGODB_URL=mongodb://127.0.0.1:27017
 # Store MongoDB data under directory ${HOME}/data/db
 mongod --fork --logpath ${HOME}/mongod.log
 
+# Start Flask application in background
+cd ${HOME}/LLM_Reasoner/src/langchain
+python flask_graph.py &
+FLASK_PID=$!
+
+# Give Flask some time to start
+sleep 5
+
+# Return to original directory
+cd ${HOME}/knowrob_binder
+
 # jupyterlab UI workspace
 jupyter lab workspaces import ${PWD}/binder/jupyterlab.jupyterlab-workspace
+
+# Function to cleanup background processes on exit
+cleanup() {
+    echo "Cleaning up background processes..."
+    kill $FLASK_PID 2>/dev/null
+    mongod --shutdown 2>/dev/null
+    exit 0
+}
+
+# Trap signals to cleanup
+trap cleanup SIGTERM SIGINT
 
 exec "$@"
